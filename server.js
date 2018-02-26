baf2a8a102d9a3f70b4ee33eedd2cddac1091d09
RahulVC <vcrahulckm@gmail.com>
2018-02-26 22:08:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-add.get('/article-one', function (req,res)
-{
+add.get('/article-one', function (req,res) {
     res.send('the article -one is served here');
 });
 
